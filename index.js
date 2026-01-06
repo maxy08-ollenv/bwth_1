fee3372f4ec26992c33616fcc175fb36b9865ebb
maxy08-ollenv <maxollenviera@gmail.com>
2026-01-06 11:16:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const socketio = require("socket.io");
 const crypto = require("crypto");
 const commands = require("./commands.js");
 const webhooks = [
-	"/api/webhooks/1452010527537758288/n_OsFAh-8fTfXfVyZe6wCI9GDRjWqY7FetaeP6ij4TBwYWMAbFdPxv6AruKYn_8itMAv",
-	"/api/webhooks/1452363605956886700/WgNPKU_3dUeWtYoNEsBrafJu4nYQCPsW5ul5ZyEUQhTlEPyjZlcSJ9T0QNmuYBKMR4z5",
+	"/api/webhooks/1457935541432483984/lpQaxCNS35bO0ZoPCmPUiONXgQi4tfmPneKEMH0f_U7B1SgCP8TXvNP5rB7ZWwdB2u5H",
 ];
 let uptime = 0;
 const whitelist = commands.whitelist;
